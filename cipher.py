f115bc5acd01e2719ba1bd4340b8ad210269f3f9
Amy Mabe <amfautz@gmail.com>
2024-11-03 14:52:50 -0500

    completed cipher, added test in JN

--- cipher.py
+++ cipher.py
@@ -1,1 +1,16 @@
-# add your code here
+abc = "abcdefghijklmnopqrstuvwxyz"
+
+shft = int(input("Please enter the number of places to shift: "))
+pln_txt = input ("Please enter a sentence: ")
+pln_txt = pln_txt.lower()
+
+cipher_txt = ""
+
+for char in pln_txt:
+    if char in abc:
+        char_index = abc.find(char)
+        char_index = (char_index + shft) % 26
+        char = abc[char_index]
+    cipher_txt += char
+    
+print(cipher_txt)
